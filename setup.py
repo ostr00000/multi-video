77dfd272a956479de0216f194929c2100276b0b4
ostro <ostr00000@gmail.com>
2019-10-30 00:12:52 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='multi-vlc',
+    version=0.2,
+    python_requires='>=3.7',
+    description='Tool to run many vlc at same time on early prepared position.',
+    packages=find_packages(exclude="*test*"),
+    entry_points={'console_scripts': [
+        'multi-vlc = multi_vlc.starter:main',
+    ]},
+    install_requires=['PyQt5', 'decorator'],
+)
