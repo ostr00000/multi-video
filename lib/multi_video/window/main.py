1a25741de455f62417ad41c778bf4a4e99a8fcf5
ostr00000 <ostr00000@gmail.com>
2021-07-08 01:07:05 +0200

    improve logger in loadPackages

--- lib/multi_video/window/main.py
+++ lib/multi_video/window/main.py
@@ -9,7 +9,8 @@ from pyqt_utils.python.dynamic_loader import loadClassFromPackage
 
 logger = logging.getLogger(__name__)
 # noinspection PyTypeChecker
-classes = list(loadClassFromPackage(multi_video.managers, BaseVideoWindow))
+classes = list(loadClassFromPackage(
+    multi_video.managers, requiredSubclass=BaseVideoWindow, logger=logger))
 
 
 class VideoWindow(*classes, BaseVideoWindow,
